8f3b2a45bb6a267b341c6188613d08c5a575bb71
Zenbook Eduardo <aeduardoiran@gmail.com>
2025-03-13 12:41:08 -0700

    Se creo endpoint para obtener propuestas de unico usuario

--- src/routes/propuestasRoutes.js
+++ src/routes/propuestasRoutes.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import {
     obtenerPropuesta,
-    crearPropuesta
+    crearPropuesta, obtenerPropuestasUsuario
 } from "../controllers/propuestasRoutes.js";
 import { verificarToken } from "../middlewares/token.js";
 
 const router = express.Router();
 
 router.get('/getProposals', obtenerPropuesta);
+router.get('/getProposals/:id', obtenerPropuestasUsuario);
 router.post('/createProposal', verificarToken, crearPropuesta)
 
 export default router;
