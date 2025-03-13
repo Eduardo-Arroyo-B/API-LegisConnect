dbb4c19a814d5912edd6dfbac2ba927ea9eda7f4
Zenbook Eduardo <aeduardoiran@gmail.com>
2025-03-13 08:53:00 -0700

    Cors

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ app.set('port', process.env.PORT || 3000)
 
 // Middlewares
 app.use(cors({
-    origin: " http://localhost:5174",
+    origin: "http://localhost:5174",
     credentials: true,
 }))
 app.use(express.json())
