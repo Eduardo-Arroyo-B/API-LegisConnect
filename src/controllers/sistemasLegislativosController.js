9caf7d722902882d256e9a409830a451213f6f41
Zenbook Eduardo <aeduardoiran@gmail.com>
2025-03-13 16:59:55 -0700

    Configuracion de await

--- src/controllers/sistemasLegislativosController.js
+++ src/controllers/sistemasLegislativosController.js
@@ -3,10 +3,16 @@ const proyecto = async (req,res) => {
     const url = 'https://datos.congreso.gov.py/opendata/api/data/proyecto'
 
     try {
-        const response = fetch(url)
-        res.json(response)
+        const response =  await fetch(url)
+
+        if (!response.ok) {
+            console.log("Ha ocurrido un error al extraer los datos")
+        }
+
+        return res.json(response)
+
     } catch (error) {
-        res.status(500).json({error: error.message})
+        res.status(500).json({ message: error.message })
     }
 }
 
